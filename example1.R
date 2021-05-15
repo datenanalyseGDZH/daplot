bef433613eafc44dd1595dc06e5237e1fb9c7b1f
Damian Brunold <damian.brunold@bluewin.ch>
2021-05-15 12:01:12 +0200

    Add comment to example1

--- example1.R
+++ example1.R
@@ -4,12 +4,17 @@
 library(ggplot2)
 library(daplot)
 
+# Example saved as PNG
+
 page(format = "slide", theme = get_slide_theme()) %>%
   add_slide_title("Example1") %>%
   add_slide_subtitle("A simple plot of speed vs distance") %>%
   add_plot_box(ggplot(data = cars) + geom_point(aes(x = speed, y = dist)),
                "default", rect = c(10, 28, 150, 110)) %>%
   save_png("example1")
+
+
+# Example saved as PDF
 
 page(format = "slide", theme = get_slide_theme()) %>%
   add_slide_title("Example1") %>%
